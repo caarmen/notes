282ad5150141bd88add20624da1c16695760d9ac
Carmen Alvarez <carmen@rmen.ca>
2023-10-15 14:58:04 +0200

    Generate doc with apispec
    Add swagger-ui template files based on https://dev.to/sanjan/how-to-add-swagger-ui-to-a-plain-flask-api-project-with-an-openapi-specification-file-1jl8

--- flask/app/main.py
+++ flask/app/main.py
@@ -5,7 +5,14 @@ from sqlalchemy.exc import InvalidRequestError
 
 from app.database.session import CONNECTION_URL, init_db
 from app.errors import handle_invalid_request_error, handle_validation_error
-from app.views import NoteGroupApi, NoteItemApi
+from app.views import (
+    NoteGroupApi,
+    NoteItemApi,
+    get_openapi,
+    get_redoc,
+    get_swagger_ui,
+    spec,
+)
 from flask import Flask
 
 
@@ -23,8 +30,17 @@ def create_app(config: AppConfig = AppConfig(db_connection_url=CONNECTION_URL)):
     app.register_error_handler(ValidationError, handle_validation_error)
     app.register_error_handler(InvalidRequestError, handle_invalid_request_error)
 
-    app.add_url_rule("/notes/", view_func=NoteGroupApi.as_view("note-group"))
-    app.add_url_rule("/notes/<int:id>", view_func=NoteItemApi.as_view("note-detail"))
+    note_item_view = NoteItemApi.as_view("note-detail")
+    note_group_view = NoteGroupApi.as_view("note-group")
+    app.add_url_rule("/notes/", view_func=note_group_view)
+    app.add_url_rule("/notes/<int:id>", view_func=note_item_view)
+    app.add_url_rule("/openapi.json", view_func=get_openapi)
+    app.add_url_rule("/redoc", view_func=get_redoc)
+    app.add_url_rule("/docs", view_func=get_swagger_ui)
+
+    with app.test_request_context():
+        spec.path(view=note_group_view)
+        spec.path(view=note_item_view)
 
     return app
 
